7381fa8c0b39332aea240b463886f122deca3227
CJMolloy <cillian.molloy@yahoo.ie>
2017-05-02 13:13:02 +0100

    Memento Created, Memento Code not Created, Proxy waiting

---
+++ CA/Guardian.java
@@ -0,0 +1,20 @@
+public class Guardian {
+
+	private int name;
+	private int state;
+
+	/**
+	 * 
+	 * @param name
+	 */
+	public void setWardName(Ward name) {
+		// TODO - implement Guardian.setWardName
+		throw new UnsupportedOperationException();
+	}
+
+	public Ward createWard() {
+		// TODO - implement Guardian.createWard
+		throw new UnsupportedOperationException();
+	}
+
+}
---
+++ CA/Ward.java
@@ -0,0 +1,26 @@
+public class Ward {
+
+	private int name;
+	private int state;
+
+	public void setName() {
+		// TODO - implement Ward.setName
+		throw new UnsupportedOperationException();
+	}
+
+	public void getName() {
+		// TODO - implement Ward.getName
+		throw new UnsupportedOperationException();
+	}
+
+	public void stateGet() {
+		// TODO - implement Ward.stateGet
+		throw new UnsupportedOperationException();
+	}
+
+	public void stateSet() {
+		// TODO - implement Ward.stateSet
+		throw new UnsupportedOperationException();
+	}
+
+}
